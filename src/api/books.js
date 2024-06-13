671c54b55255bcc1fb9636826aa7f034b77fb001
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2024-06-13 22:32:59 +0200

    add error handling to standard books component

--- src/api/books.js
+++ src/api/books.js
@@ -3,16 +3,17 @@ const BOOKS_MOCK = [
   { id: 2, name: "The Lord of the Rings" },
 ];
 
-const pseudoRanodmDelay = () => Math.floor(Math.random() * 8) * 1000;
+const pseudoRanodmDelay = () => Math.floor(Math.random() * 4) * 1000;
 
 export const fetchBooks = () => {
-  return new Promise((res, rej) =>
+  return new Promise((res, rej) => {
     setTimeout(() => {
-      // if (Math.random() < 0.5) res(BOOKS_MOCK);
-      // rej('Error while fetching books')
-      
-      res(BOOKS_MOCK);
-    }, pseudoRanodmDelay())
+        if (Math.random() < 0.5) {
+          res(BOOKS_MOCK);
+        } else {
+          rej('Error while fetching books')
+        }
+      }, pseudoRanodmDelay())}
   );
 };
 
