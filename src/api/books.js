301347de26060990a7621aa743844d1df6133762
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2024-06-30 23:12:52 +0200

    update api

--- src/api/books.js
+++ src/api/books.js
@@ -1,45 +1,21 @@
 
-const getBooksApiAuthor = author => `https://openlibrary.org/search/authors.json?q=${author}`;
+const getBooksApiByTitle = ({ title, limit }) => `https://openlibrary.org/search.json?title=${title}&limit=${2}`;
 
-const getBooksApiAuthorWorks = authorId => `https://openlibrary.org/authors/${authorId}/works.json`;
-
-
-export const AUTHOR_NOT_FOUND_CODE = 'AUTHOR NOT FOUND';
 export const BOOKS_NOT_FOUND_CODE = 'BOOKS NOT FOUND';
 
-export const fetchAuthors = async ({ author }) => {
-  const authorRes = await fetch(getBooksApiAuthor(author), { method: 'GET' });
-  const authorData = await authorRes.json();
+export const fetchBooksByTitle = async (title, { signal } = {}) => {
+  const booksRes = await fetch(getBooksApiByTitle({ title, limit: 2 }), { method: 'GET', signal  });
+  const booksData = await booksRes.json();
 
-  if (authorData.numFound === 0) {
-    throw { status: 404, code: AUTHOR_NOT_FOUND_CODE };
-  }
-
-  return authorData.docs;
-};
-
-export const fetchBooks = async ({ authorId }) => {
-  const authorWorksRes = await fetch(getBooksApiAuthorWorks(authorId), { method: 'GET' });
-  const authorWorksData = await authorWorksRes.json();
-
-  if (authorWorksData.size === 0) {
+  if (booksData.numFound === 0) {
     throw { status: 404, code: BOOKS_NOT_FOUND_CODE };
   }
 
-  return authorWorksData.entries;
+  return booksData.docs;
 };
 
-export const fetchBooksByAuthor = async (author = 'platon') => {
-  const authors = await fetchAuthors({ author });
 
-  const firstAuthorId = authors[0].key;
-
-  const books = await fetchBooks({ authorId: firstAuthorId });
-
-  return books.slice(0, 2);
-}
-
-export const fetchBooksData = () => wrapPromise(fetchBooks());
+export const fetchBooksData = () => wrapPromise(fetchBooksByTitle());
 
 function wrapPromise(promise) {
   let status = "pending";
