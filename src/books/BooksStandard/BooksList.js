64fd1de7599598f652ab7951ce0ff3288942d418
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2024-07-06 21:13:29 +0200

    add simple transition for loaded image

--- src/books/BooksStandard/BooksList.js
+++ src/books/BooksStandard/BooksList.js
@@ -1,3 +1,22 @@
-const BooksList = ({ books }) => books.map((book) => (<li key={book.key}>{book.title}</li>));
+import { useState } from 'react';
+
+const BookCoverImg = ({ isbn }) => {
+    const [loaded, setLoaded] = useState(false);
+    const handleOnLoad = () => {
+        setLoaded(true);
+    };
+
+    const style = loaded ? { opacity: 1 } : { opacity: 0 }; 
+
+    return <img style={{ ...style, ...{ transition: '1s' }}} onLoad={handleOnLoad} src={`https://covers.openlibrary.org/b/isbn/${isbn}-L.jpg`} width={150} height={200} />
+}
+
+const BooksList = ({ books }) => {
+    return books.map((book) => (
+        <li key={book.key}>
+            {book.isbn && <BookCoverImg isbn={book.isbn[0]}  />}
+            {book.title}
+        </li>
+))};
 
 export default BooksList;
