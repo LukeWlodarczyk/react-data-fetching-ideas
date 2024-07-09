b029c007a878c18a8ab0fd9afa2cebcd3998c953
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2024-07-09 20:31:27 +0200

    update Booklist styles

--- src/ui/BooksList/BooksList.js
+++ src/ui/BooksList/BooksList.js
@@ -6,7 +6,7 @@ const BooksList = ({ books }) => {
     return (
         <ul className={styles.list} >
             {books.map((book) => (
-                <li key={book.key} style={{ margin: '8px', display: 'flex', justifyContent: 'center' }}>
+                <li key={book.key} className={styles.item}>
                     <BookCard book={book}/>
                 </li>
             ))}
