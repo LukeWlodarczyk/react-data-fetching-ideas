0bb6fa97dde0ceacf8210e5d086851f57ed31d88
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2024-07-09 20:19:37 +0200

    add guard for authors

--- src/ui/BookCard/BooksCard.js
+++ src/ui/BookCard/BooksCard.js
@@ -7,13 +7,12 @@ const BookCard = ({ book }) => (
         <BookCoverImg coverId={book.cover_i}  />
         <div className={styles.description}>
             <h4 className={styles.title}>{book.title} <span className={styles.year}>({book.first_publish_year})</span></h4>
-            <p className={styles.authors}>{book.author_name.join(', ')}</p>
+            {book.author_name && <p className={styles.authors}>{book.author_name.join(', ')}</p>}
             {book.subject && <div className={styles.subjects}>
                 {book.subject.slice(0, 3).map(subject => <div className={styles.subject}>{subject}</div>)}
             </div>}
         </div>
     </div>
 )
-
 
 export default BookCard;
