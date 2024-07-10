7da572cf4a7830435745aea5d7923377e619a82d
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2024-07-10 05:33:33 +0200

    add animation for loading cover img

--- src/ui/BookCoverImg/BookCoverImg.js
+++ src/ui/BookCoverImg/BookCoverImg.js
@@ -6,15 +6,18 @@ import BookCoverImgPlaceholder from '../BookCoverImgPlaceholder';
 
 const BookCoverImg = memo(({ coverId }) => {
     const [isLoaded, setIsLoaded] = useState(false);
+    const [isError, setIsError] = useState(false);
 
     const handleOnLoad = () => setIsLoaded(true);
+    const handleOnError = () => setIsError(true);
 
     return (
-        <div className={styles.wrapper}>
+        <div className={styles.wrapper} style={{ ...(isError && { animationIterationCount: 1 }) }}>
             <BookCoverImgPlaceholder className={styles.placeholder} style={{ opacity: isLoaded ? 0 : 1 }} />
             <img
                 loading='lazy'
                 onLoad={handleOnLoad}
+                onError={handleOnError}
                 src={`https://covers.openlibrary.org/b/id/${coverId}-M.jpg?default=false`} 
                 className={styles.cover}
                 style={{ opacity: isLoaded ? 1 : 0 }} 
