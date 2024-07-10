06f7e5635b32e057db18515a6cfa0ab159947332
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2024-07-10 17:29:52 +0200

    add publish_year guard

--- src/ui/BookCard/BooksCard.js
+++ src/ui/BookCard/BooksCard.js
@@ -6,7 +6,7 @@ const BookCard = ({ book }) => (
     <div className={styles.wrapper}>
         <BookCoverImg coverId={book.cover_i}  />
         <div className={styles.description}>
-            <h4 className={styles.title}>{book.title} <span className={styles.year}>({book.first_publish_year})</span></h4>
+            <h4 className={styles.title}>{book.title} {book.first_publish_year && <span className={styles.year}>({book.first_publish_year})</span>}</h4>
             {book.author_name && <p className={styles.authors}>{book.author_name.join(', ')}</p>}
             {book.subject && <div className={styles.subjects}>
                 {book.subject.slice(0, 3).map(subject => <div key={subject} className={styles.subject}>{subject}</div>)}
