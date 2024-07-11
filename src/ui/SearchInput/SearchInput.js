610e388f9bd0d322a2e8d74a92f9d31e98417dbc
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2024-07-11 15:18:56 +0200

    add input icons and animations

--- src/ui/SearchInput/SearchInput.js
+++ src/ui/SearchInput/SearchInput.js
@@ -1,7 +1,13 @@
 import styles from './SearchInput.module.css'
 
-const SearchInput = ({ ...props }) => {
-    return <input className={styles.search} {...props}/>
+const SearchInput = ({ isLoading, isSuccess, isEmpty, isEmptyBooks, isError, ...props }) => {
+    return (
+        <div className={styles.wrapper}>
+            <div className={`${styles.inputWrapper} ${isLoading && styles.loading} ${isSuccess && styles.success} ${isEmpty && styles.empty} ${isEmptyBooks && styles.emptyBooks} ${isError && styles.error}`}>
+                <input className={styles.search} {...props}/>
+            </div>
+            </div>
+    )
 }
 
 export default SearchInput;
