be8b70991b69e5e385afbfa21b183807982a6983
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2024-07-27 12:59:02 +0200

    add global hooks

---
+++ src/hooks/useDebounce.js
@@ -0,0 +1,17 @@
+import { useLayoutEffect, useMemo, useRef } from 'react';
+import _debounce from 'lodash/debounce';
+
+const useDebounce = (callback, delay) => {
+    const callbackRef = useRef(callback)
+
+    useLayoutEffect(() => {
+        callbackRef.current = callback
+    })
+
+    return useMemo(
+        () => _debounce((...args) => callbackRef.current(...args), delay),
+        [delay],
+    )
+};
+
+export default useDebounce;
