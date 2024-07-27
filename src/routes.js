3e0674a27a00947975bb2c48938a55f8ef12474f
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2024-07-27 07:17:10 +0200

    add route preload

--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,20 @@ import { lazy } from "react";
 import Home from './Home';
 import NotFound from './NotFound';
 
-const BooksStandard = lazy(() => import('./books/BooksStandard/BooksStandard'));
-const BooksStandard2 = lazy(() => import('./books/BooksStandard2/BooksStandard'));
-const BooksUseSWR = lazy(() => import('./books/BooksUseSWR'));
-const BooksUseQuery = lazy(() => import('./books/BooksUseQuery'));
-const BooksResourceSuspene = lazy(() => import('./books/BooksResourceSuspene'));
-const BooksUseSuspenseSWR = lazy(() => import('./books/BooksUseSuspenseSWR'));
-const BooksUseSuspenseQuery = lazy(() => import('./books/BooksUseSuspenseQuery'));
-const BooksUse = lazy(() => import('./books/BooksUse'));
+const lazyWithPreload = cb => {
+    const Component = lazy(cb);
+    Component.preload = cb;
+    return Component;
+};
+
+const BooksStandard = lazyWithPreload(() => import('./books/BooksStandard/BooksStandard'));
+const BooksStandard2 = lazyWithPreload(() => import('./books/BooksStandard2/BooksStandard'));
+const BooksUseSWR = lazyWithPreload(() => import('./books/BooksUseSWR'));
+const BooksUseQuery = lazyWithPreload(() => import('./books/BooksUseQuery'));
+const BooksResourceSuspene = lazyWithPreload(() => import('./books/BooksResourceSuspene'));
+const BooksUseSuspenseSWR = lazyWithPreload(() => import('./books/BooksUseSuspenseSWR'));
+const BooksUseSuspenseQuery = lazyWithPreload(() => import('./books/BooksUseSuspenseQuery'));
+const BooksUse = lazyWithPreload(() => import('./books/BooksUse'));
 
 export const BASIC_ROUTES = [
   {
@@ -29,42 +35,50 @@ export const BOOKS_ROUTES = [
   {
     path: '/no-lib-1',
     name: 'No libs #1',
-    element: <BooksStandard />
+    element: <BooksStandard />,
+    preload: BooksStandard.preload,
   },
   {
     path: '/no-lib-2',
     name: 'No libs #2',
-    element: <BooksStandard2 />
+    element: <BooksStandard2 />,
+    preload: BooksStandard2.preload,
   },
   {
     path: '/swr',
     name: 'SWR',
-    element: <BooksUseSWR />
+    element: <BooksUseSWR />,
+    preload: BooksUseSWR.preload,
   },
   {
     path: '/tanstack-query',
     name: 'TanStack Query',
-    element: <BooksUseQuery />
+    element: <BooksUseQuery />,
+    preload: BooksUseQuery.preload,
   },
   {
     path: '/resource-suspense',
     name: 'Resource Suspense',
-    element: <BooksResourceSuspene />
+    element: <BooksResourceSuspene />,
+    preload: BooksResourceSuspene.preload,
   },
   {
     path: '/swr-suspense',
     name: 'SWR Suspense',
-    element: <BooksUseSuspenseSWR />
+    element: <BooksUseSuspenseSWR />,
+    preload: BooksUseSuspenseSWR.preload,
   },
   {
     path: '/tanstack-query-suspense',
     name: 'TanStack Query Suspense',
-    element: <BooksUseSuspenseQuery />
+    element: <BooksUseSuspenseQuery />,
+    preload: BooksUseSuspenseQuery.preload,
   },
   {
     path: '/reac-use',
     name: 'React.use',
-    element: <BooksUse />
+    element: <BooksUse />,
+    preload: BooksUse.preload,
   },
 ];
 
