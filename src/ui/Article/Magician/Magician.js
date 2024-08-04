3ee99d578eb5d3f8f766b7bc34fe4b45e41ef75d
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2024-08-04 21:27:32 +0200

    add animation to magician icon when its in view

--- src/ui/Article/Magician/Magician.js
+++ src/ui/Article/Magician/Magician.js
@@ -1,10 +1,17 @@
+import { useInView } from 'react-intersection-observer';
+import cn from 'classnames';
+
 import { ReactComponent as MagicianIcon } from './svg/magician.svg';
 
+import styles from './Magician.module.css';
 
 const Magician = () => {
+  const { ref, inView } = useInView({ rootMargin: '-30% 0% -30% 0%' });
 
   return (
     <MagicianIcon
+      ref={ref}
+      className={cn(styles.magician, { [styles.animate]: inView })}
     />
   );
 };
