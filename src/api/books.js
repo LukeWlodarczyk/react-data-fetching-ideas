872ea644fdfc6b8508f89926e100007bb407e117
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2024-08-05 23:41:33 +0200

    remove cache option

--- src/api/books.js
+++ src/api/books.js
@@ -2,22 +2,15 @@ import axios from 'axios';
 
 import { BASE_URL } from './constants';
 
-import memoize from './memoize';
-
 const getBooksByTitleApiUrl = ({ title, limit }) =>
   `${BASE_URL}/search.json?title=${title}&limit=${limit}`;
 
 const request = ({ title }, { signal }) =>
   axios(getBooksByTitleApiUrl({ title, limit: 3 }), { signal });
-const mRequest = memoize(request, ({ title }) => title);
 
-export const fetchBooksByTitle = async (
-  title,
-  { signal, cacheEnabled = false } = {}
-) => {
-  const fetcher = cacheEnabled ? mRequest : request;
 
-  const res = await fetcher({ title }, { signal });
+export const fetchBooksByTitle = async (title, { signal } = {}) => {
+  const res = await request({ title }, { signal });
 
   return res.data.docs;
 };
