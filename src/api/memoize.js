73733ccd455d842461b6ea63d70d9ad85a458ddd
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2024-08-06 12:26:25 +0200

    add cleanup method to memoize

--- src/api/memoize.js
+++ src/api/memoize.js
@@ -1,22 +1,37 @@
 const memoize = (fetcher, resolver = (...args) => args[0]) => {
   const cacheMap = new Map();
 
-  return (...args) => {
+  const memoized = (...args) => {
     const key = resolver(...args);
 
     const cached = cacheMap.get(key);
 
     if (cached) return cached;
 
-    const promise = fetcher(...args).catch((e) => {
-      cacheMap.delete(key);
-      throw e;
-    });
+    const promise = fetcher(...args)
+      .then((data) => {
+        promise.isResolved = true;
+        return data;
+      })
+      .catch((e) => {
+        cacheMap.delete(key);
+        throw e;
+      });
 
     cacheMap.set(key, promise);
 
     return promise;
   };
+
+  const cleanup = (key) => {
+    const cached = cacheMap.get(key);
+
+    if (!cached?.isResolved) cacheMap.delete(key);
+  };
+
+  memoized.cleanup = cleanup;
+
+  return memoized;
 };
 
 export default memoize;
