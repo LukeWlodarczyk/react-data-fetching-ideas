9947c3420c9c341eb446aa2ce3e96f7b7560230a
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2024-08-11 19:15:24 +0200

    refactor to adjust to new BookCoverImgPlaceholder

--- src/ui/BookCoverImg/BookCoverImg.js
+++ src/ui/BookCoverImg/BookCoverImg.js
@@ -1,29 +1,38 @@
 import { useState, memo } from 'react';
 
-import styles from './BookCoverImg.module.css'
+import cn from 'classnames';
 
 import BookCoverImgPlaceholder from '../BookCoverImgPlaceholder';
 
+import styles from './BookCoverImg.module.css';
+
+const getImgSrc = (coverId) =>
+  `https://covers.openlibrary.org/b/id/${coverId}-M.jpg?default=false`;
+
 const BookCoverImg = memo(({ coverId }) => {
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [isError, setIsError] = useState(false);
+  const hasCoverId = coverId !== undefined;
 
-    const handleOnLoad = () => setIsLoaded(true);
-    const handleOnError = () => setIsError(true);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [isError, setIsError] = useState(!hasCoverId);
 
-    return (
-        <div className={styles.wrapper} style={{ ...(isError && { animationIterationCount: 1 }) }}>
-            <BookCoverImgPlaceholder className={styles.placeholder} style={{ opacity: isLoaded ? 0 : 1 }} />
-            <img
-                loading='lazy'
-                onLoad={handleOnLoad}
-                onError={handleOnError}
-                src={`https://covers.openlibrary.org/b/id/${coverId}-M.jpg?default=false`} 
-                className={styles.cover}
-                style={{ opacity: isLoaded ? 1 : 0 }} 
-            />
-        </div>
-    )
+  const handleOnLoad = () => setIsLoaded(true);
+  const handleOnError = () => setIsError(true);
+
+  const isLoading = !isLoaded && !isError;
+
+  return (
+    <div className={styles.wrapper}>
+      <BookCoverImgPlaceholder isLoading={isLoading} isDefault={isError} />
+      {hasCoverId && (
+        <img
+          className={cn(styles.cover, { [styles.show]: isLoaded })}
+          onLoad={handleOnLoad}
+          onError={handleOnError}
+          src={getImgSrc(coverId)}
+        />
+      )}
+    </div>
+  );
 });
 
 export default BookCoverImg;
