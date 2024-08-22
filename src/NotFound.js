3f310b0ba6e853ef1071e126d90a18fd78c6b5d2
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2024-08-22 16:44:02 +0200

    update NotFound page

--- src/NotFound.js
+++ src/NotFound.js
@@ -1,7 +1,15 @@
-export default function NotFound() {
-  return (
-        <header>
-          <h2>NotFound</h2>
-        </header>
-  );
-}
+import Page from '@/ui/Page';
+import Header from '@/ui/Header';
+import PageHeading from '@/ui/PageHeading';
+import Icon from '@/ui/Icon';
+
+const NotFound = () => (
+  <Page>
+    <Header>
+      <Icon name="cleaningMan" size="large" />
+      <PageHeading level="2">Page Not Found</PageHeading>
+    </Header>
+  </Page>
+);
+
+export default NotFound;
