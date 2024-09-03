c48e202b7ac393ec8edd44d0a8154faf0c5eb5aa
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2024-09-03 19:51:42 +0200

    add Window ui component for CodeEditor

---
+++ src/ui/CodeEditor/Window/Window.js
@@ -0,0 +1,24 @@
+import { PanelGroup, Panel, PanelResizeHandle } from 'react-resizable-panels';
+import styles from './Window.module.css';
+
+const Window = ({ main, sidePanel, header }) => (
+  <article className={styles.window}>
+    <header className={styles.header}>{header}</header>
+    <PanelGroup className={styles.content} direction="horizontal">
+      <Panel
+        className={styles.sidepanel}
+        defaultSize={30}
+        minSize={10}
+        collapsible
+      >
+        {sidePanel}
+      </Panel>
+      <PanelResizeHandle />
+      <Panel className={styles.main} defaultSize={70} minSize={50}>
+        {main}
+      </Panel>
+    </PanelGroup>
+  </article>
+);
+
+export default Window;
