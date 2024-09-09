8c466fa93cc69f04894c0e449ab596135496b1ab
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2024-09-09 13:23:33 +0200

    add util filterInactiveModules for CodeEditor

--- src/ui/CodeEditor/utils.js
+++ src/ui/CodeEditor/utils.js
@@ -6,3 +6,18 @@ export const minLinesNumber = (code, minNumberOfLines) => {
   if (isMinimum) return code;
   else return code + '\n'.repeat(minNumberOfLines - numberOfLines);
 };
+
+const MODULES_DIRECTORY_NAME = 'modules';
+
+export const filterInactiveModules = (nodes, activeModuleNodeName) => {
+  return nodes.map((node) =>
+    node.name === MODULES_DIRECTORY_NAME
+      ? {
+          ...node,
+          nodes: node.nodes.filter(
+            (subnode) => subnode.name === activeModuleNodeName
+          ),
+        }
+      : node
+  );
+};
